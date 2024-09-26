28e77b63c611d45127c7434970effd87c9fe72c1
cmrohityadav <cmrohityadav23@gmail.com>
2024-09-26 23:52:57 +0530

    variable

---
+++ 02_variable/cp_01_.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+
+
+int main(){
+
+    std::cout<<"welcome to Variable"<<std::endl;
+    int iMktPrice=1000;
+    double dAmountBalance=40000;
+
+
+    
+    return 0;
+}
---
+++ 02_variable/cp_02_const.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    
+    const int iAadhar=123145697852;
+    
+    return 0;
+}
