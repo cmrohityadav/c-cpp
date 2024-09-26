4ef5d053ed09a7b1b827f0cf25b9498649472315
cmrohityadav <cmrohityadav23@gmail.com>
2024-09-26 23:52:11 +0530

    hellow word and namespace

---
+++ 01_basic/cp_01_helloWorld.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+using std::cout;
+namespace rohitRegion{
+    void Display(){
+        //anything
+    }
+}
+int main()
+{
+    
+    cout<<"hello Rohit"<<endl;
+    rohitRegion::Display();
+    std::cout<<"best way to use"<<std::endl;
+    return 0;
+}
