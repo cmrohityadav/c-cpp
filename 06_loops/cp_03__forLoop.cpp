0feaf29c8eb8a24c79b6bbaed59263dd0683139b
cmrohityadav <cmrohityadav23@gmail.com>
2024-10-02 23:40:08 +0530

    loops

---
+++ 06_loops/cp_03__forLoop.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{   
+    cout<<"Enter the Number of Cups"<<endl;
+    int iTeaCups;
+    cin>>iTeaCups;
+
+    for(int iTeaCounter=1;iTeaCounter<=iTeaCups;iTeaCounter++ ){
+
+        cout<<"lemon tea cup "<<iTeaCounter<<" of tea...\n";
+
+    }
+    
+    return 0;
+}
