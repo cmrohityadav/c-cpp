efff70bbc71121e2de082b237e8081697aafa67d
Rohit Yadav <cmrohityadav23@gmail.com>
2025-12-20 23:52:57 +0530

    added default constructor

--- clab/main.cpp
+++ clab/main.cpp
@@ -1,12 +1,22 @@
 #include<iostream>
-#include<iterator>
+class Strategy{
+    public:
+    int qty;
+    int price;
+    std::string userName;
+
+    Strategy(){
+        qty=10;
+        price=10.5;
+        userName="TraderRohit";
+    }
+
+};
 
 int main(){
-    int fruits[]={1,2,3,4,5,6};
-
-    int count {std::size(fruits)};
-
-
-    std::cout<<count<<std::endl;
+    Strategy s;
+    std::cout<<s.price<<std::endl;
+    std::cout<<s.qty<<std::endl;
+    std::cout<<s.userName<<std::endl;
     return 0;
 }
