e40fc01cf87256b86bf68ce8f5a8b723f2412c73
Rohit Yadav <cmrohityadav23@gmail.com>
2025-12-21 00:21:06 +0530

    added about parametric constructor

--- clab/main.cpp
+++ clab/main.cpp
@@ -2,19 +2,19 @@
 class Strategy{
     public:
     int qty;
-    int price;
+    double price;
     std::string userName;
 
-    Strategy(){
-        qty=10;
-        price=10.5;
-        userName="TraderRohit";
+    Strategy(int orderQty,double orderPrice,std::string userID){
+       qty=orderQty;
+       price=orderPrice;
+       userName=userID;
     }
 
 };
 
 int main(){
-    Strategy s;
+    Strategy s(10,10.5,"traderrohit");
     std::cout<<s.price<<std::endl;
     std::cout<<s.qty<<std::endl;
     std::cout<<s.userName<<std::endl;
