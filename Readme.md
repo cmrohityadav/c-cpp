0ab8e4f4da333bcfa98739ae4ee2ffccf1f4142e
Rohit Yadav <cmrohityadav23@gmail.com>
2025-12-27 00:20:36 +0530

    added Passing Values to a Thread Function

--- Readme.md
+++ Readme.md
@@ -1231,6 +1231,44 @@ int main(){
 g++ -pthread main.cpp
 ```
 
+### Passing Values to a Thread Function
+```cpp
+#include<iostream>
+#include<thread>
+#include<chrono>
+
+void quantumCalculation(int a, int b){
+    int msec=0;
+    while(msec<100){
+        std::cout<<"Calculating Quantum Addition...."<<std::endl;
+        std::this_thread::sleep_for(std::chrono::milliseconds(1));
+        msec++;
+    }
+
+    std::cout<<"Final calculation: "<<a+b<<std::endl;
+}
+
+void quantumCalculationMul(int a, int b){
+    int msec=0;
+    while(msec<500){
+        std::cout<<"Calculating Quantum Multiply...."<<std::endl;
+        std::this_thread::sleep_for(std::chrono::milliseconds(1));
+        msec++;
+    }
+
+    std::cout<<"Final calculation Multiply: "<<a*b<<std::endl;
+}
+int main(){
+
+    std::thread t1(quantumCalculation,10,10);
+    std::thread t2(quantumCalculationMul,50,10);
+
+    t1.join();
+    t2.join();
+    return 0;
+}
+```
+
 
 
 
--- clab/main.cpp
+++ clab/main.cpp
@@ -2,39 +2,35 @@
 #include<thread>
 #include<chrono>
 
-void function1(){
-    std::cout<<"Hello from Function1"<<std::endl;
-    
-    std::this_thread::sleep_for(std::chrono::seconds(5));
+void quantumCalculation(int a, int b){
+    int msec=0;
+    while(msec<100){
+        std::cout<<"Calculating Quantum Addition...."<<std::endl;
+        std::this_thread::sleep_for(std::chrono::milliseconds(1));
+        msec++;
+    }
 
-    std::cout << "Function1 finished" << std::endl;
+    std::cout<<"Final calculation: "<<a+b<<std::endl;
 }
 
-void function2(){
-    std::cout<<"Hello from Function2"<<std::endl;
-}
-void function3(){
-    std::cout<<"Hello from Function3"<<std::endl;
+void quantumCalculationMul(int a, int b){
+    int msec=0;
+    while(msec<500){
+        std::cout<<"Calculating Quantum Multiply...."<<std::endl;
+        std::this_thread::sleep_for(std::chrono::milliseconds(1));
+        msec++;
+    }
+
+    std::cout<<"Final calculation Multiply: "<<a*b<<std::endl;
 }
 int main(){
 
-    std::thread t1(function1);
-    std::cout<<"Thread t1 created"<<std::endl;
+    std::thread t1(quantumCalculation,10,10);
+    std::thread t2(quantumCalculationMul,50,10);
 
-    std::thread t2(function2);
-    std::cout<<"Thread t2 created"<<std::endl;
-
-    t1.join(); //WAIT POINT
-    std::cout<<"Join t1"<<std::endl;
-
-    std::thread t3(function3);
-    std::cout<<"Thread t3 created"<<std::endl;
-    t3.join();
-    std::cout<<"Join t3"<<std::endl;
-
-
+    t1.join();
     t2.join();
-    std::cout<<"Join t2"<<std::endl;
+
 
 
     return 0;
