0d2d4cab026e712408ef937aae1f0b829e430da1
Rohit Yadav <cmrohityadav23@gmail.com>
2026-08-22 21:19:57 +0530

    added livelock

--- Threading.md
+++ Threading.md
@@ -733,7 +733,80 @@ int main() {
 ```
 
 ### Livelock
+- Dono threads chal rahe hote hain, repeatedly action bhi le rahe hote hain, lekin actual kaam complete nahi hota
+```cpp
+#include <iostream>
+#include <thread>
+#include <mutex>
+#include <chrono>
 
+std::mutex m1;
+std::mutex m2;
+
+void thread1() {
+    while (true) {
+        m1.lock();
+
+        std::cout << "Thread 1 got m1\n";
+
+        // Dusra mutex try karo
+        if (m2.try_lock()) {
+            std::cout << "Thread 1 got m2\n";
+            std::cout << "Thread 1 working\n";
+
+            m2.unlock();
+            m1.unlock();
+
+            break;
+        }
+        else {
+            std::cout << "Thread 1: m2 busy, releasing m1\n";
+
+            m1.unlock();
+
+            std::this_thread::sleep_for(
+                std::chrono::milliseconds(100)
+            );
+        }
+    }
+}
+
+void thread2() {
+    while (true) {
+        m2.lock();
+
+        std::cout << "Thread 2 got m2\n";
+
+        // Dusra mutex try karo
+        if (m1.try_lock()) {
+            std::cout << "Thread 2 got m1\n";
+            std::cout << "Thread 2 working\n";
+
+            m1.unlock();
+            m2.unlock();
+
+            break;
+        }
+        else {
+            std::cout << "Thread 2: m1 busy, releasing m2\n";
+
+            m2.unlock();
+
+            std::this_thread::sleep_for(
+                std::chrono::milliseconds(100)
+            );
+        }
+    }
+}
+
+int main() {
+    std::thread t1(thread1);
+    std::thread t2(thread2);
+
+    t1.join();
+    t2.join();
+}
+```
 ### Starvation
 
 
--- clab/main.cpp
+++ clab/main.cpp
@@ -7,47 +7,65 @@ std::mutex m1;
 std::mutex m2;
 
 void thread1() {
-    m1.lock();
+    while (true) {
+        m1.lock();
 
-    std::cout << "Thread 1 locked m1\n";
+        std::cout << "Thread 1 got m1\n";
 
-    // Thread 2 ko m2 lock karne ka chance diya
-    std::this_thread::sleep_for(std::chrono::seconds(1));
+        // Dusra mutex try karo
+        if (m2.try_lock()) {
+            std::cout << "Thread 1 got m2\n";
+            std::cout << "Thread 1 working\n";
 
-    std::cout << "Thread 1 waiting for m2\n";
+            m2.unlock();
+            m1.unlock();
 
-    m2.lock();  // yahan wait karega
+            break;
+        }
+        else {
+            std::cout << "Thread 1: m2 busy, releasing m1\n";
 
-    std::cout << "Thread 1 working\n";
+            m1.unlock();
 
-    m2.unlock();
-    m1.unlock();
+            std::this_thread::sleep_for(
+                std::chrono::milliseconds(100)
+            );
+        }
+    }
 }
 
 void thread2() {
-    m2.lock();
+    while (true) {
+        m2.lock();
 
-    std::cout << "Thread 2 locked m2\n";
+        std::cout << "Thread 2 got m2\n";
 
-    // Thread 1 ko m1 lock karne ka chance diya
-    std::this_thread::sleep_for(std::chrono::seconds(1));
+        // Dusra mutex try karo
+        if (m1.try_lock()) {
+            std::cout << "Thread 2 got m1\n";
+            std::cout << "Thread 2 working\n";
 
-    std::cout << "Thread 2 waiting for m1\n";
+            m1.unlock();
+            m2.unlock();
 
-    m1.lock();  // yahan wait karega
+            break;
+        }
+        else {
+            std::cout << "Thread 2: m1 busy, releasing m2\n";
 
-    std::cout << "Thread 2 working\n";
+            m2.unlock();
 
-    m1.unlock();
-    m2.unlock();
+            std::this_thread::sleep_for(
+                std::chrono::milliseconds(100)
+            );
+        }
+    }
 }
 
 int main() {
-    std::thread threadOneObject(thread1);
-    std::thread threadTwoObject(thread2);
+    std::thread t1(thread1);
+    std::thread t2(thread2);
 
-    threadOneObject.join();
-    threadTwoObject.join();
-
-    return 0;
+    t1.join();
+    t2.join();
 }
