2824871697511e671bab152a40f3976cd998ea5a
Rohit Yadav <cmrohityadav23@gmail.com>
2026-08-26 23:35:13 +0530

    clab: added recursive_timed_mutex

--- Threading.md
+++ Threading.md
@@ -455,6 +455,70 @@ int main(){
 ```
 
 ### recursive_timed_mutex
+- Same thread mutex ko multiple times lock kar sakta hai, aur agar mutex kisi doosre thread ke paas locked hai, to specified time tak wait karega. Agar given time mein lock nahi mila, to false return karega
+```cpp
+#include <iostream>
+#include <thread>
+#include <mutex>
+#include <chrono>
+
+using std::cout;
+
+std::recursive_timed_mutex mtx;
+
+void hello(int i, int threadId)
+{
+    cout << "Thread " << threadId
+         << " -> Trying to get lock...\n";
+
+    if (mtx.try_lock_for(std::chrono::seconds(3)))
+    {
+        cout << "Thread " << threadId
+             << " -> 🔒 Lock acquired | Hello : " << i << '\n';
+
+        // Same thread recursively locking the same mutex
+        if (i < 5)
+        {
+            hello(i + 1, threadId);
+        }
+
+        // Just to make Thread 2 wait and show timed behaviour
+        if (i == 1)
+        {
+            std::this_thread::sleep_for(std::chrono::seconds(5));
+        }
+
+        mtx.unlock();
+
+        cout << "Thread " << threadId
+             << " -> 🔓 Lock released\n";
+    }
+    else
+    {
+        cout << "Thread " << threadId
+             << " -> ⏰ Timeout! Could not get lock\n";
+    }
+}
+
+int main()
+{
+    std::thread t1(hello, 1, 1);
+
+    // Give Thread 1 a small head start
+    std::this_thread::sleep_for(std::chrono::milliseconds(100));
+
+    std::thread t2(hello, 1, 2);
+    std::thread t3(hello, 1, 3);
+
+    t1.join();
+    t2.join();
+    t3.join();
+
+    return 0;
+}
+
+```
+
 ### shared_mutex
 ### shared_timed_mutex
 
--- clab/main.cpp
+++ clab/main.cpp
@@ -1,48 +1,59 @@
-#include<iostream>
-#include<thread>
-#include<mutex>
-#include<chrono>
-using  std::cout;
+#include <iostream>
+#include <thread>
+#include <mutex>
+#include <chrono>
 
-std::timed_mutex mtx;
+using std::cout;
 
-void task(int id){
+std::recursive_timed_mutex mtx;
 
-    if(mtx.try_lock_for(std::chrono::seconds(15))){ //max. 15 sec tak wait karega lock lene k liye
+void hello(int i, int threadId)
+{
+    cout << "Thread " << threadId
+         << " -> Trying to get lock...\n";
 
-        cout<<"Thread "<<id<<" got the lock\n";
+    if (mtx.try_lock_for(std::chrono::seconds(3)))
+    {
+        cout << "Thread " << threadId
+             << " -> 🔒 Lock acquired | Hello : " << i << '\n';
 
-        std::this_thread::sleep_for(std::chrono::seconds(3));
+        // Same thread recursively locking the same mutex
+        if (i < 5)
+        {
+            hello(i + 1, threadId);
+        }
 
-        cout<<"Thread "<<id<<"  unlocking....\n";
+        // Just to make Thread 2 wait and show timed behaviour
+        if (i == 1)
+        {
+            std::this_thread::sleep_for(std::chrono::seconds(5));
+        }
 
         mtx.unlock();
 
-
-    }else{
-       std::cout << "Thread " << id << " timeout!\n";
+        cout << "Thread " << threadId
+             << " -> 🔓 Lock released\n";
+    }
+    else
+    {
+        cout << "Thread " << threadId
+             << " -> ⏰ Timeout! Could not get lock\n";
     }
 }
 
-int main(){
+int main()
+{
+    std::thread t1(hello, 1, 1);
 
-    std::thread t1(task,1);
-    std::thread t2(task,2);
-    std::thread t3(task,3);
-    std::thread t4(task,4);
-    std::thread t5(task,5);
-    std::thread t6(task,6);
-    std::thread t7(task,7);
-    std::thread t8(task,8);
-    
+    // Give Thread 1 a small head start
+    std::this_thread::sleep_for(std::chrono::milliseconds(100));
+
+    std::thread t2(hello, 1, 2);
+    std::thread t3(hello, 1, 3);
+
     t1.join();
     t2.join();
     t3.join();
-    t4.join();
-    t5.join();
-    t6.join();
-    t7.join();
-    t8.join();
 
     return 0;
 }
