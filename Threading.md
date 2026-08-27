d91c1740fb14d9fc8bdb8a85a671971d039ee2fc
Rohit Yadav <cmrohityadav23@gmail.com>
2026-08-27 22:59:53 +0530

    clad: added shared_timed_mutex

--- Threading.md
+++ Threading.md
@@ -604,6 +604,99 @@ int main()
 ```
 
 ### shared_timed_mutex
+```cpp
+#include <iostream>
+#include <thread>
+#include <shared_mutex>
+#include <chrono>
+
+std::shared_timed_mutex mtx;
+
+int sharedData = 100;
+
+void reader(int id)
+{
+    std::cout << "Reader " << id
+              << " -> Trying to get shared lock...\n";
+
+    if (mtx.try_lock_shared_for(
+            std::chrono::seconds(10)))
+    {
+        std::cout << "Reader " << id
+                  << " -> Shared lock acquired\n";
+
+        std::cout << "Reader " << id
+                  << " -> Reading value: "
+                  << sharedData << "\n";
+
+        std::this_thread::sleep_for(
+            std::chrono::seconds(2)
+        );
+
+        mtx.unlock_shared();
+
+        std::cout << "Reader " << id
+                  << " -> Shared lock released:Unlock\n";
+    }
+    else
+    {
+        std::cout << "Reader " << id
+                  << " -> Timeout! Could not get shared lock\n";
+    }
+}
+
+void writer(int id)
+{
+    std::cout << "Writer " << id
+              << " -> Trying to get exclusive lock...\n";
+
+    if (mtx.try_lock_for(
+            std::chrono::seconds(3)))
+    {
+        std::cout << "Writer " << id
+                  << " -> Exclusive lock acquired\n";
+
+        sharedData += 10;
+
+        std::this_thread::sleep_for(
+            std::chrono::seconds(5)
+        );
+
+        std::cout << "Writer " << id
+                  << " -> Updated value: "
+                  << sharedData << "\n";
+
+        mtx.unlock();
+
+        std::cout << "Writer " << id
+                  << " -> Exclusive lock released\n";
+    }
+    else
+    {
+        std::cout << "Writer " << id
+                  << " -> Timeout! Could not get exclusive lock\n";
+    }
+}
+
+int main()
+{
+    std::thread w1(writer, 1);
+    std::thread r1(reader, 1);
+    std::this_thread::sleep_for(
+        std::chrono::milliseconds(100)
+    );
+    std::thread w2(writer, 2);
+
+    std::thread r2(reader, 2);
+
+    w1.join();
+    w2.join();
+    r1.join();
+    r2.join();
+
+    return 0;
+}
+```
 
 ## Locking Mechanisms
 ### std::lock_guard
--- clab/main.cpp
+++ clab/main.cpp
@@ -1,73 +1,91 @@
 #include <iostream>
 #include <thread>
-#include <mutex>
 #include <shared_mutex>
 #include <chrono>
 
-int sharedData = 0;
+std::shared_timed_mutex mtx;
 
-std::shared_mutex mtx;
+int sharedData = 100;
 
 void reader(int id)
 {
-    mtx.lock_shared();
-
     std::cout << "Reader " << id
-              << " is reading: "
-              << sharedData << "\n";
+              << " -> Trying to get shared lock...\n";
 
-    std::this_thread::sleep_for(
-        std::chrono::seconds(2)
-    );
+    if (mtx.try_lock_shared_for(
+            std::chrono::seconds(10)))
+    {
+        std::cout << "Reader " << id
+                  << " -> Shared lock acquired\n";
 
-    mtx.unlock_shared();
+        std::cout << "Reader " << id
+                  << " -> Reading value: "
+                  << sharedData << "\n";
 
-    std::cout << "Reader " << id
-              << " finished reading\n";
+        std::this_thread::sleep_for(
+            std::chrono::seconds(2)
+        );
+
+        mtx.unlock_shared();
+
+        std::cout << "Reader " << id
+                  << " -> Shared lock released:Unlock\n";
+    }
+    else
+    {
+        std::cout << "Reader " << id
+                  << " -> Timeout! Could not get shared lock\n";
+    }
 }
 
 void writer(int id)
 {
-    mtx.lock();
-
-    std::cout << "\nWriter " << id
-              << " is writing...\n";
-
-    sharedData++;
-
-    std::this_thread::sleep_for(
-        std::chrono::seconds(2)
-    );
-
     std::cout << "Writer " << id
-              << " updated value to: "
-              << sharedData << "\n\n";
+              << " -> Trying to get exclusive lock...\n";
 
-    mtx.unlock();
+    if (mtx.try_lock_for(
+            std::chrono::seconds(3)))
+    {
+        std::cout << "Writer " << id
+                  << " -> Exclusive lock acquired\n";
+
+        sharedData += 10;
+
+        std::this_thread::sleep_for(
+            std::chrono::seconds(5)
+        );
+
+        std::cout << "Writer " << id
+                  << " -> Updated value: "
+                  << sharedData << "\n";
+
+        mtx.unlock();
+
+        std::cout << "Writer " << id
+                  << " -> Exclusive lock released\n";
+    }
+    else
+    {
+        std::cout << "Writer " << id
+                  << " -> Timeout! Could not get exclusive lock\n";
+    }
 }
 
 int main()
 {
-    std::thread r1(reader, 1);
-    std::thread r2(reader, 2);
     std::thread w1(writer, 1);
-
-    std::this_thread::sleep_for(std::chrono::seconds(10));
-    
+    std::thread r1(reader, 1);
+    std::this_thread::sleep_for(
+        std::chrono::milliseconds(100)
+    );
     std::thread w2(writer, 2);
-    std::thread r3(reader, 3);
-    std::thread r4(reader, 4);
 
+    std::thread r2(reader, 2);
 
-
-
-
+    w1.join();
+    w2.join();
     r1.join();
     r2.join();
-    r3.join();
-    w1.join();
-    r4.join();
-    w2.join();
 
     return 0;
 }
