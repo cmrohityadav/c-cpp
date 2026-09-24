2049b83f375fc7e08cef236671c0e934f4f42350
Rohit Yadav <cmrohityadav23@gmail.com>
2026-09-24 23:11:53 +0530

    added harware concurrency

--- clab/main.cpp
+++ clab/main.cpp
@@ -1,41 +1,9 @@
 #include <iostream>
 #include <thread>
-#include <semaphore>
-#include <chrono>
-
-using namespace std::chrono_literals;
-
-std::binary_semaphore entry_token(0);
-
-void organizer()
-{
-    std::cout << "[Organizer] Waiting for special entry...\n";
-
-    std::this_thread::sleep_for(3s);
-
-    std::cout << "[Organizer] Entry token issued!\n";
-
-    // Token available
-    entry_token.release();
-}
-
-void guest()
-{
-    std::cout << "[Guest] Waiting for entry token...\n";
-
-    // Token nahi hai -> BLOCK
-    entry_token.acquire();
-
-    std::cout << "[Guest] Token received. Entering event...\n";
-}
 
 int main()
 {
-    std::thread t1(organizer);
-    std::thread t2(guest);
+    unsigned int count = std::thread::hardware_concurrency();
 
-    t1.join();
-    t2.join();
-
-    return 0;
+    std::cout << "Hardware concurrency: "<< count << '\n';
 }
